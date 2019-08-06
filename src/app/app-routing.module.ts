909eecb2469551ae5a1f146c48e71fd445761310
Nontokozo <nontokozo92kunene@gmail.com>
2019-08-06 13:20:01 +0200

    added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component'
 import { AboutUsComponent} from './about-us/about-us.component'
 import { ContactUsComponent } from './contact-us/contact-us.component'
 import { RegisterComponent } from './register/register.component';
+import { VideoComponent } from './video/video.component';
 
 const routes: Routes = [
 { path: 'home', component: HomeComponent},
 { path: 'about-us', component: AboutUsComponent},
 { path: 'contact-us', component: ContactUsComponent},
-{ path: 'register', component: RegisterComponent}
+{ path: 'register', component: RegisterComponent},
+{ path: 'video', component: VideoComponent}
 ];
 
 @NgModule({
