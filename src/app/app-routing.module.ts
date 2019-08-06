14e0aa34d35aaf23e929a85a1a337a63e80dc2b9
Nontokozo <nontokozo92kunene@gmail.com>
2019-08-06 10:00:46 +0200

    edits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 import { AboutUsComponent} from './about-us/about-us.component'
 import { ContactUsComponent } from './contact-us/contact-us.component'
-import { RegisterComponent} from './register/register.component'
-
 
 const routes: Routes = [
 { path: 'home', component: HomeComponent},
 { path: 'about-us', component: AboutUsComponent},
-{ path: 'contact-us', component: ContactUsComponent},
-{ path: 'register', component: RegisterComponent}
+{ path: 'contact-us', component: ContactUsComponent}
 ];
 
 @NgModule({
